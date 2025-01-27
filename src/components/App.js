d98af3f2d4e263a3f8aa20b0dc5e2035a67bbafd
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-01-27 14:49:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        
+        <Routes>
+          <Route path="/" element={<ItemList />} />
+          <Route path="/item/:id" element={<ItemDetail />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
 
 import React from "react";
 import './../styles/App.css';
